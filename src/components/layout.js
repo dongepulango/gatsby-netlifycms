cf7d907ed39f52967acf14350f53b9e8c4bfe313
Don Gepulango <dongepulango89@gmail.com>
2019-09-02 17:16:44 +0800

    Added Favicons

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,6 @@
 import React from 'react';
+//helmet
+import { Helmet } from "react-helmet";
 //style
 import styled, { createGlobalStyle } from 'styled-components';
 import vars from './varss';
@@ -7,6 +9,12 @@ import typography from './typography';
 //components
 import Header from "./header";
 import Footer from "./footer";
+//favicons
+import favicon from "../img/favicon/favicon.ico";
+import favicon16 from "../img/favicon/favicon-16x16.png";
+import favicon32 from "../img/favicon/favicon-32x32.png";
+import faviconApple from "../img/favicon/apple-touch-icon.png";
+import faviconSafari from "../img/favicon/safari-pinned-tab.svg";
 
 //Reset & Default Styles
 const GlobalStyle = createGlobalStyle`
@@ -24,6 +32,13 @@ const Main = styled.main`
 const Layout = (props) => {
   return (
     <React.Fragment>
+      <Helmet>
+        <link rel="apple-touch-icon" sizes="180x180" href={faviconApple} />
+        <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+        <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+        <link rel="shortcut icon" href={favicon} />
+        <link rel="mask-icon" href={faviconSafari} color="#5bbad5" />
+      </Helmet>
       <GlobalStyle />
       <Header />
       <Main>
